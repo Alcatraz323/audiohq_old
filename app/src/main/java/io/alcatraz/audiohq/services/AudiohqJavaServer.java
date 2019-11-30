37ea32256beea492f12be4002c4220673235abde
Alcatraz323 <alcatraz32323@gmail.com>
2019-11-30 15:23:58 +0800

    2019_11_30_15_23_version_0.4_reconstruct_before_java_server

---
+++ app/src/main/java/io/alcatraz/audiohq/services/AudiohqJavaServer.java
@@ -0,0 +1,20 @@
+package io.alcatraz.audiohq.services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+public class AudiohqJavaServer extends Service{
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
